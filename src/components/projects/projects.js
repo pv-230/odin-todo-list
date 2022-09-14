4385f05ba7383cda708fd3ef0c07ef3d93872d19
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-09-13 22:19:07 -0400

    Add ability to open/close sidebar

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -11,6 +11,21 @@ const displayProjList = (state) => {
     projectsList.removeChild(projectsList.lastChild);
   }
 
+  // Adds an input to allow the user to add new projects
+  const newItem = document.createElement('li');
+  newItem.classList.add('projects__new-item');
+
+  const newItemInput = document.createElement('input');
+  newItemInput.classList.add('projects__new-item-input');
+  newItemInput.placeholder = 'New Project';
+
+  const addBtn = document.createElement('div');
+  addBtn.classList.add('projects__add-btn');
+
+  newItem.appendChild(newItemInput);
+  newItem.appendChild(addBtn);
+  projectsList.appendChild(newItem);
+
   // Adds all existing projects to the sidebar
   state.projArr.forEach((proj, i) => {
     const listItem = document.createElement('li');
@@ -32,21 +47,60 @@ const displayProjList = (state) => {
     listItem.appendChild(deleteBtn);
     projectsList.appendChild(listItem);
   });
-
-  // Adds an input to allow the user to add new projects
-  const newItem = document.createElement('li');
-  newItem.classList.add('projects__new-item');
-
-  const newItemInput = document.createElement('input');
-  newItemInput.classList.add('projects__new-item-input');
-  newItemInput.placeholder = 'New Project';
-
-  const addBtn = document.createElement('div');
-  addBtn.classList.add('projects__add-btn');
-
-  newItem.appendChild(newItemInput);
-  newItem.appendChild(addBtn);
-  projectsList.appendChild(newItem);
 };
+
+/**
+ * Event listener for the menu button (show/hide projects)
+ */
+const menuBtn = document.querySelector('.projects__menu-btn');
+menuBtn.addEventListener('click', () => {
+  if (menuBtn.classList.contains('projects__menu-btn_open')) {
+    menuBtn.classList.replace(
+      'projects__menu-btn_open',
+      'projects__menu-btn_closed'
+    );
+
+    const projects = document.querySelector('.projects');
+    projects.classList.add('projects_closed');
+
+    const projectInfo = document.querySelector(
+      '.tasks__project-info',
+      HTMLElement
+    );
+    projectInfo.style.padding = '20px 20px 20px 85px';
+
+    const menuGroup = document.querySelector('.projects__title-group');
+    menuGroup.classList.add('projects__title-group_closed');
+
+    const menuTitle = document.querySelector('.projects__menu-title');
+    menuTitle.classList.add('hidden');
+
+    const projectsList = document.querySelector('.projects__list');
+    projectsList.classList.add('hidden');
+  } else {
+    menuBtn.classList.replace(
+      'projects__menu-btn_closed',
+      'projects__menu-btn_open'
+    );
+
+    const projects = document.querySelector('.projects');
+    projects.classList.remove('projects_closed');
+
+    const projectInfo = document.querySelector(
+      '.tasks__project-info',
+      HTMLElement
+    );
+    projectInfo.style.padding = '20px';
+
+    const menuGroup = document.querySelector('.projects__title-group');
+    menuGroup.classList.remove('projects__title-group_closed');
+
+    const menuTitle = document.querySelector('.projects__menu-title');
+    menuTitle.classList.remove('hidden');
+
+    const projectsList = document.querySelector('.projects__list');
+    projectsList.classList.remove('hidden');
+  }
+});
 
 export { displayProjList };
